22f23652e5bef6943fcdc5d3fcb4700d49bdc02f
Prime <jeff.d.937@gmail.com>
2021-09-24 11:17:38 -0400

    " try"

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+inquirer
+  .prompt([
+      {
+          type: 'input' .
+          name: 'name'  ,
+          message: ''
+      },
+      
+      {
+        type: 'input' .
+        name: 'name'  ,
+        message: ''
+    },
+    {
+        type: 'input' .
+        name: 'name'  ,
+        message: ''
+    },
+    {
+        type: 'input' .
+        name: 'name'  ,
+        message: ''
+    },
+    {
+        type: 'input' .
+        name: 'name'  ,
+        message: ''
+    },
+    {
+        type: 'input' .
+        name: 'name'  ,
+        message: ''
+    },
+    {
+        type: 'input' .
+        name: 'name'  ,
+        message: ''
+    },
+    {
+        type: 'input' .
+        name: 'name'  ,
+        message: ''
+    },
+    {
+        type: 'input' .
+        name: 'name'  ,
+        message: ''
+    },
+  ]) 
